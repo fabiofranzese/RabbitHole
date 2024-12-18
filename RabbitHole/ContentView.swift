9e725a3ad6dc1404f7fd213fcf3ec53687f00c6d
Fabio Franzese <fabiofranzeseuni@gmail.com>
2024-12-18 17:06:22 +0100

    Initial Commit

---
+++ RabbitHole/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RabbitHole
+//
+//  Created by Fabio on 18/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RabbitHole/RabbitHoleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RabbitHoleApp.swift
+//  RabbitHole
+//
+//  Created by Fabio on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RabbitHoleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
